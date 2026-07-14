5bb3f1ef42c06f9f4fcd5f1ab4d15bea04ba61b9
MFR Rayhan <md.fazlerabbirayhan786@gmail.com>
2026-07-14 17:58:33 +0600

    Refactor AuthLayout component: enhance layout with welcome message and adjust flex properties

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,8 +1,10 @@
 export default function AuthLayout({ children }) {
   return (
-    <div className="flex gap-10">
-      <div className="flex-1">{children}</div>
-      <div className="flex-1">Register form</div>
+    <div className="flex min-h-screen gap-10">
+      <div className="flex-2">{children}</div>
+      <div className="flex items-center flex-1 text-5xl bg-sky-300 text-black rounded p-10">
+        <h2>Welcome for Authentication</h2>
+      </div>
     </div>
   );
 }
