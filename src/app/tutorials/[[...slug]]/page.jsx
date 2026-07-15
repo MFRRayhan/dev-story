862d444e3b15a2a455e33dc61274b048bc9b22bc
MFR Rayhan <md.fazlerabbirayhan786@gmail.com>
2026-07-15 07:20:28 +0600

    Update Tutorials page to display dynamic content based on slug parameters

--- src/app/tutorials/[[...slug]]/page.jsx
+++ src/app/tutorials/[[...slug]]/page.jsx
@@ -3,10 +3,17 @@ import Title from "@/components/Title";
 export default async function Tutorials({ params }) {
   const { slug } = await params;
   console.log(slug);
+  const [technology, topic, , page_no, subject] = slug;
 
   return (
     <div>
-      <Title>Tutorials</Title>
+      <Title>{technology} Tutorials</Title>
+      <h2>{topic}</h2>
+      <div className="divider"></div>
+      <div className="flex justify-between">
+        <h3>{subject}</h3>
+        <p>Page No. {page_no}</p>
+      </div>
     </div>
   );
 }
