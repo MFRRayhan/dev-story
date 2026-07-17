448d166f6f3cb16fb9080a69de98491b112e04d4
MFR Rayhan <md.fazlerabbirayhan786@gmail.com>
2026-07-17 18:53:59 +0600

    Change loading animation icon from anchor to target

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="flex items-center justify-center text-8xl min-h-screen">
       <h2>
-        L <span className="animate-spin">⚓ </span>
+        L <span className="animate-spin">🎯 </span>
       </h2>
       <h2>ding...</h2>
     </div>
